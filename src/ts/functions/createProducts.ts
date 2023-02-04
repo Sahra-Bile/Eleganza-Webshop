e40f17aa6ac47fefe2b3132773068a19af7f1b1c
Sahra <sahra.bile134@gmail.com>
2023-02-04 10:06:01 +0100

    lite loop ändring

--- src/ts/functions/createProducts.ts
+++ src/ts/functions/createProducts.ts
@@ -10,7 +10,7 @@ export function createHTMLForProducts() {
 
   mainProducts.innerHTML = "";
 
-  for (let i = 0; i < ProductDetails.length; i++) {
+  for (let i = ProductDetails.length - 1; i >= 0; i--) {
     let productDiv: HTMLDivElement = document.createElement("div");
 
     let imgWrapper: HTMLDivElement = document.createElement("div");
--- src/ts/models/productData.ts
+++ src/ts/models/productData.ts
@@ -5,7 +5,7 @@ export let ProductDetails: Product[] = [
     1,
     "Rich Dad Poor Dad",
     "https://s1.adlibris.com/images/28226190/rich-dad-poor-dad.jpg",
-    "Rich Dad Poor Dad is Robert's story of growing up with two dads his real father and the father of his best friend, his rich dad  and the ways in which both men shaped his thoughts about money and investing.",
+    "Rich Dad Poor Dad is Robert's story of growing up with two dads his real father and the father of his best friend, his rich dad  and the ways in which both men shaped his thoughts about money and investing",
     150
   ),
   new Product(
@@ -13,7 +13,7 @@ export let ProductDetails: Product[] = [
     "Me Before you ",
     "https://s1.adlibris.com/images/78628/me-before-you.jpg",
 
-    "Lou Clark knows lots of things. She knows how many footsteps there are between the bus stop and home. She knows she likes working in The Buttered Bun tea shop and she knows she might not love her boyfriend Patrick. What Lou doesn't know is she's about to lose her job or that knowing what's  ",
+    "Lou Clark knows lots of things. She knows how many footsteps there are between the bus stop and home. She knows she likes working in The Buttered Bun tea shop and she knows she might not love her boyfriend Patrick. What Lou doesn't know is she's about to lose her job or that knowing what's",
     200
   ),
 
@@ -21,7 +21,7 @@ export let ProductDetails: Product[] = [
     3,
     "Where the Crawdads Sing",
     "https://s2.adlibris.com/images/49550148/where-the-crawdads-sing.jpg",
-    "Perfect for fans of Barbara Kingsolver and Celeste Ng, Where the Crawdads Sing is at once an exquisite ode to the natural world, a heartbreaking coming-of-age story, and a surprising tale of possible murder.",
+    "Perfect for fans of Barbara Kingsolver and Celeste Ng, Where the Crawdads Sing is at once an exquisite ode to the natural world, a heartbreaking coming-of-age story, and a surprising tale of possible murder",
     250
   ),
 
@@ -29,7 +29,7 @@ export let ProductDetails: Product[] = [
     4,
     "Gone  Girl ",
     "https://s2.adlibris.com/images/574787/gone-girl.jpg",
-    "'What are you thinking, Amy? The question I've asked most often during our marriage, if not out loud, if not to the person who could answer. ",
+    "'What are you thinking, Amy? The question I've asked most often during our marriage, if not out loud, if not to the person who could answer",
     199
   ),
 
@@ -37,22 +37,22 @@ export let ProductDetails: Product[] = [
     5,
     "12 Rules for Life",
     "https://s1.adlibris.com/images/41715874/12-rules-for-life.jpg",
-    "Acclaimed psychologist Jordan Peterson has become one of the most influential public thinkers to emerge on the world stage for many years, with his lectures on topics from the Bible to romantic relationships to mythology drawing an unprecedented following of tens of millions of viewers.",
+    "Acclaimed psychologist Jordan Peterson has become one of the most influential public thinkers to emerge on the world stage for many years, with his lectures on topics from the Bible to romantic relationships to mythology drawing an unprecedented following of tens of millions of viewers",
     300
   ),
 
   new Product(
     6,
-    " The Girl on the Train ",
+    " The Girl on the Train",
     "https://s1.adlibris.com/images/23855003/the-girl-on-the-train-a-novel---reviewed.jpg",
-    "Rachel catches the same commuter train every morning. She knows it will wait at the same signal each time, overlooking a row of back gardens. She's even started to feel like she knows the people who live in one of the houses. 'Jess and Jason', she calls them. Their life - as she sees it - is perfect.",
+    "Rachel catches the same commuter train every morning. She knows it will wait at the same signal each time, overlooking a row of back gardens. She's even started to feel like she knows the people who live in one of the houses. 'Jess and Jason', she calls them. Their life - as she sees it - is perfect",
     200
   ),
   new Product(
     7,
     "It Starts with Us",
     "https://s2.adlibris.com/images/62721802/it-starts-with-us.jpg",
-    "Before It Ends with Us, it started with Atlas. Colleen Hoover tells fan favourite Atlas' side of the story and shares what comes next in this long-anticipated sequel to the #1 Sunday Times bestseller It Ends with Us.",
+    "Before It Ends with Us, it started with Atlas. Colleen Hoover tells fan favourite Atlas' side of the story and shares what comes next in this long-anticipated sequel to the #1 Sunday Times bestseller It Ends with Us",
     220
   ),
   new Product(
